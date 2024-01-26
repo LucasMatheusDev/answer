0ba3260239c585ef31feb01c2e41f89e7c197297
LucasMatheusDev <lucasmatheusdev@gmail.com>
2024-01-26 12:13:54 -0300

    Melhorias na tratativa de erros em AnswerFailure
    Aprimoramos a classe AnswerFailure adicionando documentação para clarificar o uso do padrão de erro e a opção de registro de logs. Agora, é possível especificar o uso da stack trace atual quando nenhuma é fornecida explicitamente e também recomendamos desativar o log em ambientes de produção para melhorar a gestão de erros e a performance das aplicações.

--- lib/src/answer_failure.dart
+++ lib/src/answer_failure.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+/// Pattern error failure
 abstract class AnswerFailure implements Exception {
   final String message;
   final int code;
@@ -9,8 +10,12 @@ abstract class AnswerFailure implements Exception {
   AnswerFailure({
     required this.message,
     required this.code,
+
+    /// When [stackTrace] is null, the current stack trace is used.
     this.stackTrace,
     this.error,
+
+    /// Recommended set to [false] in production
     bool enableLog = true,
   }) {
     if (enableLog) {
